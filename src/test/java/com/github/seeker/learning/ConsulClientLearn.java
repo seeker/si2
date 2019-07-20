59e4cbf297bb765bf822cbdb0aa62400f95f5491
Nicholas Wright <dozedoffagain@gmail.com>
2019-07-20 17:05:10 +0200

    Learning test to get node information

---
+++ src/test/java/com/github/seeker/learning/ConsulClientLearn.java
@@ -0,0 +1,54 @@
+package com.github.seeker.learning;
+
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+
+
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+import com.google.common.net.HostAndPort;
+import com.orbitz.consul.AgentClient;
+import com.orbitz.consul.Consul;
+import com.orbitz.consul.HealthClient;
+import com.orbitz.consul.model.agent.ImmutableRegistration;
+import com.orbitz.consul.model.agent.Registration;
+import com.orbitz.consul.model.health.ServiceHealth;
+
+public class ConsulClientLearn {
+	private static Consul client;
+	private static AgentClient serviceAgent;
+	
+	private static final String MONGODB_SERVICE = "mongodb";
+	private static final String NODE_IP = "192.168.42.10";
+	
+	@BeforeClass
+	public static void beforeClass() {
+		client = Consul.builder().withHostAndPort(HostAndPort.fromParts(NODE_IP, 8500)).build();
+		serviceAgent = client.agentClient();
+		
+		Registration service = ImmutableRegistration.builder().id(MONGODB_SERVICE).name(MONGODB_SERVICE).port(27017).build();
+		
+		serviceAgent.register(service);
+	}
+
+	@Test
+	public void getHealthyService() {
+		HealthClient healthClient = client.healthClient();
+		assertThat(healthClient.getHealthyServiceInstances(MONGODB_SERVICE).getResponse(),hasSize(1));
+	}
+	
+	@Test
+	public void getIpOfDatabase() {
+		HealthClient healthClient = client.healthClient();
+		
+		List<ServiceHealth> services  = healthClient.getHealthyServiceInstances(MONGODB_SERVICE).getResponse();
+		
+		assertThat(services.get(0).getNode().getAddress(), is(NODE_IP));
+	}
+}
