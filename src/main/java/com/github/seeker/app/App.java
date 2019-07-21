419e161056ebfc439e79a9719c111a8685b5e7ad
Nicholas Wright <dozedoffagain@gmail.com>
2019-07-21 19:04:16 +0200

    File loader can connect to RabbitMq

--- src/main/java/com/github/seeker/app/App.java
+++ src/main/java/com/github/seeker/app/App.java
@@ -4,11 +4,46 @@
  */
 package com.github.seeker.app;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
+import net.sourceforge.argparse4j.inf.Subparsers;
+
 public class App {
+	private static final String COMMAND_ATTRIBUTE = "command";
+	private static final String LOADER_COMMAND = "loader";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ArgumentParser parser  = ArgumentParsers.newFor("Si2").build();
+		Subparsers subparsers = parser.addSubparsers().title("subcommands");
+		
+		Subparser loader = subparsers.addParser("loader").description("loads files for processing").setDefault(COMMAND_ATTRIBUTE, LOADER_COMMAND);
+		loader.addArgument("--id").required(true).nargs(1).action(Arguments.store()).help("The id of this loader, used to get anchors from consul");
 
+		try {
+			processArgs(parser.parseArgs(args));
+		} catch (ArgumentParserException e) {
+			parser.handleError(e);
+			System.exit(1);
+		}
 	}
-
+	
+	private static void processArgs(Namespace namespace) {
+		System.out.println(namespace);
+		
+		if(LOADER_COMMAND.equals(namespace.getString(COMMAND_ATTRIBUTE))) {
+			try {
+				new FileLoader(namespace.getString("id"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
---
+++ src/main/java/com/github/seeker/app/FileLoader.java
@@ -0,0 +1,54 @@
+package com.github.seeker.app;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.seeker.configuration.ConfigurationBuilder;
+import com.github.seeker.configuration.ConsulClient;
+import com.github.seeker.configuration.ConsulClient.ConfiguredService;
+import com.orbitz.consul.model.health.ServiceHealth;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * Loads files from the file system and sends them to the message broker with additional meta data.
+ */
+public class FileLoader {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileLoader.class);
+	
+	private final Channel channel;
+	private final String queueFileFeed;
+	
+	public FileLoader(String id) throws IOException, TimeoutException {
+		ConsulClient consul = new ConsulClient(new ConfigurationBuilder().getConsulConfiguration());
+		ServiceHealth rabbitmqService = consul.getFirstHealtyInstance(ConfiguredService.rabbitmq);
+		
+		String serverAddress = rabbitmqService.getNode().getAddress();
+		int serverPort = rabbitmqService.getService().getPort();
+		
+		
+		ConnectionFactory connFactory = new ConnectionFactory();
+		connFactory.setUsername("si2");
+		connFactory.setPassword(consul.getKvAsString("config/rabbitmq/users/si2"));
+		connFactory.setHost(serverAddress);
+		connFactory.setPort(serverPort);
+		
+		queueFileFeed = consul.getKvAsString("config/rabbitmq/queue/loader-file-feed");
+		
+		LOGGER.info("Connecting to Rabbitmq server {}:{}", serverAddress, serverPort);
+		
+		Connection conn = connFactory.newConnection();
+		channel = conn.createChannel();
+		
+		LOGGER.info("Creating queue {}", queueFileFeed);
+		channel.queueDeclare(queueFileFeed, false, false, false, null);
+	}
+	
+	public void loadFiles() {
+		
+	}
+}
