d446637c042183ddf3f98ec82adc03f1e2300be7
Nicholas Wright <dozedoffagain@gmail.com>
2019-08-13 18:36:57 +0200

    Moved code to module

---
+++ core/src/main/java/com/github/seeker/app/Gui.java
@@ -0,0 +1,10 @@
+/* The MIT License (MIT)
+ * Copyright (c) 2019 Nicholas Wright
+ * http://opensource.org/licenses/MIT
+ */
+package com.github.seeker.app;
+
+//TODO split this into a separate module
+public class Gui {
+
+}
