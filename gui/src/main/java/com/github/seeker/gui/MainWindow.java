047ab21a5c23f46299c3b4c23d37e47dde1e9434
Nicholas Wright <dozedoffagain@gmail.com>
2019-08-16 18:49:27 +0200

    Build and run JavaFX GUI

--- gui/src/main/java/com/github/seeker/gui/MainWindow.java
+++ gui/src/main/java/com/github/seeker/gui/MainWindow.java
@@ -1,10 +1,24 @@
 package com.github.seeker.gui;
 
-public class MainWindow {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        primaryStage.setScene(scene);
+        primaryStage.show();		
+	}
 }
