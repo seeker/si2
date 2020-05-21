00051d375d39e0c96f2a3b73fca0dbab6ef4c9c1
Nicholas Wright <dozedoffagain@gmail.com>
2020-05-22 01:04:25 +0200

    Use Consul DNS to deploy to registry

--- deploy-vagrant.sh
+++ deploy-vagrant.sh
@@ -2,5 +2,5 @@
 
 # Build the Docker image with the node binary and push it to the registry
 mvn clean package -Dmaven.test.skip=true
-docker build -t 10.0.2.15:5000/si2-node:latest .
-docker push 10.0.2.15:5000/si2-node:latest
+docker build -t docker-registry.service.consul:5000/si2-node:latest .
+docker push docker-registry.service.consul:5000/si2-node:latest
