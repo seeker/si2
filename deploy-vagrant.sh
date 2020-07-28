d49143c364a3c159af899e0d7bf39e51e7286729
Nicholas Wright <dozedoffagain@gmail.com>
2020-07-28 18:53:37 +0200

    Set ENV for dockerd in Vagrant VM

--- deploy-vagrant.sh
+++ deploy-vagrant.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Connect to dockerd in Vagrant
+export DOCKER_HOST="tcp://127.0.0.1:2375"
+
 # Build the Docker image with the node binary and push it to the registry
 mvn clean package -Dmaven.test.skip=true
 docker build -t docker-registry.service.consul:5000/si2-node:latest .
